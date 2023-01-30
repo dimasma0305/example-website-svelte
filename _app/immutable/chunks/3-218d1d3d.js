7de9c5b989a26e5372d2ab5ccd91a452b1ff880d
dimas0305 <dimasmaulana0305@gmail.com>
2023-01-30 09:32:14 +0800

    Updates

---
+++ _app/immutable/chunks/3-218d1d3d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/x/_...path_/_page.svelte-bdbf3770.js";export{t as component};
--- _app/immutable/chunks/3-749ca60b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/x/_...path_/_page.svelte-b32435ab.js";export{t as component};
